43ac4e72fabbe7e640a4f5bcec06834bfcbd6062
carlostcba <33902391+carlostcba@users.noreply.github.com>
2024-06-01 15:12:48 -0300

    Update get_data_mysql.py

--- get_data_mysql.py
+++ get_data_mysql.py
@@ -34,28 +34,37 @@ for processor in c.Win32_Processor():
 # Obtener memoria física instalada (RAM)
 for cs in c.Win32_ComputerSystem():
     MemoriaRAM = round(int(cs.TotalPhysicalMemory) / (1024**3), 2)
+    if MemoriaRAM >= 1024:  # Si la memoria es mayor o igual a 1 TB
+        MemoriaRAM = round(MemoriaRAM / 1024, 2)
+        unidad_medida = "TB"
+    elif MemoriaRAM >= 1:  # Si la memoria es mayor o igual a 1 GB
+        unidad_medida = "GB"
+    else:  # Si la memoria es menor a 1 GB
+        MemoriaRAM = round(MemoriaRAM * 1024, 2)
+        unidad_medida = "MB"
+    MemoriaRAM = f"{MemoriaRAM} {unidad_medida}"
 
 # Obtener información de cada banco de memoria
 bancos_memoria = []
 for memoria in c.Win32_PhysicalMemory():
     tamaño = round(int(memoria.Capacity) / (1024**3), 2)  # Convertir bytes a GB
     if tamaño > 0:
-        bancos_memoria.append((memoria.DeviceLocator, tamaño))
+        bancos_memoria.append(f"{tamaño} GB")
 
 # Completar hasta 4 bancos de memoria con valores nulos si es necesario
 while len(bancos_memoria) < 4:
-    bancos_memoria.append((None, None))
+    bancos_memoria.append(None)
 
 # Obtener unidades de disco
 discos = []
 for disk in c.Win32_DiskDrive():
     modelo = disk.Model
     if disk.Size is not None:
-        tamaño = round(int(disk.Size) / (1024**3), 2)
+        tamaño_gb = round(int(disk.Size) / (1024**3), 2)
+        capacidad = f"{tamaño_gb} GB"  # Agregar la unidad de medida "GB" al valor de capacidad
     else:
-        tamaño = 0  # Si el tamaño es desconocido, se establece en 0 para excluirlo
-    if tamaño > 0:
-        discos.append((modelo, tamaño))
+        capacidad = "0 GB"  # Si el tamaño es desconocido, se establece en 0 para excluirlo
+    discos.append((modelo, capacidad))
 
 # Completar hasta 4 discos con valores nulos si es necesario
 while len(discos) < 4:
@@ -64,27 +73,16 @@ while len(discos) < 4:
 # Conectar a la base de datos MySQL
 conn = mysql.connector.connect(
     host='localhost',
-    user='tu_usuario',
-    password='tu_contraseña',
-    database='inventoryDB'  # Asegúrate de usar el nombre correcto de la base de datos
+    user='inventario',
+    password='LaSalle2599',
+    database='inventoryDB'
 )
 cursor = conn.cursor()
 
 # Insertar los datos en la tabla
 query = """
-INSERT INTO inventoryLS (
-    nameOS, tipoOS, hostname, motherboard, totalRAM, 
-    bank1_name, bank1_capacity, bank2_name, bank2_capacity, 
-    bank3_name, bank3_capacity, bank4_name, bank4_capacity, 
-    disk1_model, disk1_capacity, disk2_model, disk2_capacity, 
-    disk3_model, disk3_capacity, disk4_model, disk4_capacity, date)
-VALUES (
-    %s, %s, %s, %s, %s, 
-    %s, %s, %s, %s, 
-    %s, %s, %s, %s, 
-    %s, %s, %s, %s, 
-    %s, %s, %s
-)
+INSERT INTO inventoryLS (nameOS, tipoOS, hostname, motherboard, totalRAM, bank1, bank2, bank3, bank4, disk1_model, disk1_capacity, disk2_model, disk2_capacity, disk3_model, disk3_capacity, disk4_model, disk4_capacity, date)
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
 """
 values = (
     NombreSO,
@@ -92,14 +90,18 @@ values = (
     NombreSistema,
     ProductoPlacaBase,
     MemoriaRAM,
-    bancos_memoria[0][0], bancos_memoria[0][1],
-    bancos_memoria[1][0], bancos_memoria[1][1],
-    bancos_memoria[2][0], bancos_memoria[2][1],
-    bancos_memoria[3][0], bancos_memoria[3][1],
-    discos[0][0], discos[0][1],
-    discos[1][0], discos[1][1],
-    discos[2][0], discos[2][1],
-    discos[3][0], discos[3][1],
+    bancos_memoria[0],
+    bancos_memoria[1],
+    bancos_memoria[2],
+    bancos_memoria[3],
+    discos[0][0],
+    discos[0][1],
+    discos[1][0],
+    discos[1][1],
+    discos[2][0],
+    discos[2][1],
+    discos[3][0],
+    discos[3][1],
     datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 )
 
