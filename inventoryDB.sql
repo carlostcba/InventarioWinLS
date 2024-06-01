37912855be3d021a9ea8717d1d80e7150c9f3104
carlostcba <33902391+carlostcba@users.noreply.github.com>
2024-06-01 15:14:07 -0300

    Create inventoryDB.sql

---
+++ inventoryDB.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE IF NOT EXISTS inventoryDB;
+
+USE inventoryDB;
+
+CREATE TABLE `inventoryls` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nameOS` varchar(255) NOT NULL,
+  `tipoOS` varchar(50) NOT NULL,
+  `hostname` varchar(255) NOT NULL,
+  `motherboard` varchar(255) NOT NULL,
+  `totalRAM` varchar(20) NOT NULL
+  `bank1` varchar(255) DEFAULT NULL,
+  `bank2` varchar(255) DEFAULT NULL,
+  `bank3` varchar(255) DEFAULT NULL,
+  `bank4` varchar(255) DEFAULT NULL,
+  `disk1_model` varchar(255) DEFAULT NULL,
+  `disk1_capacity` varchar(255) DEFAULT NULL,
+  `disk2_model` varchar(255) DEFAULT NULL,
+  `disk2_capacity` varchar(255) DEFAULT NULL,
+  `disk3_model` varchar(255) DEFAULT NULL,
+  `disk3_capacity` varchar(255) DEFAULT NULL,
+  `disk4_model` varchar(255) DEFAULT NULL,
+  `disk4_capacity` varchar(255) DEFAULT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
