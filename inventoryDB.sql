15c8c0d9ce572f8fe99f1762355850cbef019a0e
carlostcba <33902391+carlostcba@users.noreply.github.com>
2024-06-02 21:36:41 -0300

    Update inventoryDB.sql

--- inventoryDB.sql
+++ inventoryDB.sql
@@ -3,12 +3,13 @@ CREATE DATABASE IF NOT EXISTS inventoryDB;
 USE inventoryDB;
 
 CREATE TABLE `inventoryls` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `nameOS` varchar(255) NOT NULL,
   `tipoOS` varchar(50) NOT NULL,
   `hostname` varchar(255) NOT NULL,
   `motherboard` varchar(255) NOT NULL,
-  `totalRAM` varchar(20) NOT NULL
+  `processor` varchar(255) DEFAULT NULL,
+  `totalRAM` varchar(20) NOT NULL,
   `bank1` varchar(255) DEFAULT NULL,
   `bank2` varchar(255) DEFAULT NULL,
   `bank3` varchar(255) DEFAULT NULL,
@@ -21,6 +22,5 @@ CREATE TABLE `inventoryls` (
   `disk3_capacity` varchar(255) DEFAULT NULL,
   `disk4_model` varchar(255) DEFAULT NULL,
   `disk4_capacity` varchar(255) DEFAULT NULL,
-  `date` timestamp NOT NULL DEFAULT current_timestamp(),
-  PRIMARY KEY (`id`)
+  `date` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
