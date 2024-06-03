a820f3b68e39cbbd845f65f03d01a6c318601d01
carlostcba <33902391+carlostcba@users.noreply.github.com>
2024-06-03 00:42:31 -0300

    Update inventoryDB.sql

--- inventoryDB.sql
+++ inventoryDB.sql
@@ -22,7 +22,10 @@ CREATE TABLE `inventoryls` (
   `disk3_capacity` varchar(255) DEFAULT NULL,
   `disk4_model` varchar(255) DEFAULT NULL,
   `disk4_capacity` varchar(255) DEFAULT NULL,
-  `mac_addresses` TEXT DEFAULT NULL,
-  `ip_address` VARCHAR(15)DEFAULT NULL,
+  `mac1` VARCHAR(17), 
+  `mac2` VARCHAR(17), 
+  `mac3` VARCHAR(17), 
+  `mac4` VARCHAR(17), 
+  `ip_principal` VARCHAR(15);
   `date` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
